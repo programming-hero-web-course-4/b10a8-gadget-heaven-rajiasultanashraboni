ef574a1a9834faa96f397ee555cf7a15a7d55f53
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-10 12:43:23 +0600

    updated

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,61 +1,66 @@
-import React from 'react';
-import Heading from '../headingAndSubHeading/Heading';
-import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import React from "react";
+import Heading from "../headingAndSubHeading/Heading";
+import { NavLink, Outlet } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Dashboard = () => {
-    return (
-      <>
-        <div className="bg-[#9538e2] w-full h-[350px]">
-          <div className="pt-5 flex flex-col items-center justify-center">
-            <Heading
-              title={
-                <>
-                  <span>Dashboard</span>
-                </>
-              }
-              subtitle={
-                <>
-                  <span>
-                    Explore the latest gadgets that will take your experience to
-                    the next level. From smart devices to <br></br> the coolest
-                    accessories, we have it all!
-                  </span>
-                </>
-              }
-            ></Heading>
-          </div>
-          <div className="flex justify-center items-center gap-4 mt-6">
-            <NavLink
-              to="/dashboard/cart"
-              className={({ isActive }) =>
-                `text-xl ${
-                  isActive
-                    ? "btn w-[100px]"
-                    : "btn btn-outline border-white text-white w-[100px]"
-                }`
-              }
-            >
-              Cart
-            </NavLink>
-            <NavLink
-              to="/dashboard/wish"
-              className={({ isActive }) =>
-                `text-xl ${
-                  isActive
-                    ? "btn w-[100px]"
-                    : "btn btn-outline border-white text-white w-[100px]"
-                }`
-              }
-            >
-              Wishlist
-            </NavLink>
-          </div>
+  return (
+    <>
+      <Helmet>
+        <title>Dashboard | Shraboni's Gadget Shops</title>
+      </Helmet>
+
+      <div className="bg-[#9538e2] w-full h-[350px]">
+        <div className="pt-5 flex flex-col items-center justify-center">
+          <Heading
+            title={
+              <>
+                <span>Dashboard</span>
+              </>
+            }
+            subtitle={
+              <>
+                <span>
+                  Explore the latest gadgets that will take your experience to
+                  the next level. From smart devices to <br /> the coolest
+                  accessories, we have it all!
+                </span>
+              </>
+            }
+          ></Heading>
         </div>
-        <div>
-          <Outlet></Outlet>
+        <div className="flex justify-center items-center gap-4 mt-6">
+          <NavLink
+            to="/dashboard/cart"
+            className={({ isActive }) =>
+              `text-xl ${
+                isActive
+                  ? "btn w-[100px]"
+                  : "btn btn-outline border-white text-white w-[100px]"
+              }`
+            }
+          >
+            Cart
+          </NavLink>
+          <NavLink
+            to="/dashboard/wish"
+            className={({ isActive }) =>
+              `text-xl ${
+                isActive
+                  ? "btn w-[100px]"
+                  : "btn btn-outline border-white text-white w-[100px]"
+              }`
+            }
+          >
+            Wishlist
+          </NavLink>
         </div>
-      </>
-    );
+      </div>
+      <div>
+        <Outlet></Outlet>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
