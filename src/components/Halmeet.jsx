2775add52a039d7dc44e1e4ca6253342e8d453ab
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-10 11:50:08 +0600

    helmet is updated here

--- src/components/Halmeet.jsx
+++ src/components/Halmeet.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 
-const Halmeet = ({title}) => {
-    return (
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{title}</title>
-      </Helmet>
-    );
+const Helmet = ({ title }) => {
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>{title}</title>
+    </Helmet>
+  );
 };
 
-export default Halmeet;
+export default Helmet;
